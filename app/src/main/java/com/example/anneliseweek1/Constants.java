c3308ae8d78ac0b7e97de066aa4f3a9475b37c64
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2022-05-02 23:12:06 -0700

    gradlew fix

--- app/src/main/java/com/example/anneliseweek1/Constants.java
+++ app/src/main/java/com/example/anneliseweek1/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static final String USERNAME_KEY = "username";
     public static final String AGE_KEY = "birthday";
     public static final String NAME_KEY = "name";
-    public static final String SELECTED_DOB = "selectedDob";
+    public static final String SELECTED_DOB = "selectedBirthdate";
 }
